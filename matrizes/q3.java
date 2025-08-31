075ef445758de9ff76e4c4d106a0b73b2ba1ff8f
SamuelSous0 <samuel.00000859887@unicap.br>
2025-08-30 21:44:46 -0300

    Update q3.java

--- matrizes/q3.java
+++ matrizes/q3.java
@@ -2,5 +2,19 @@ public class q3 {
     public static void main(String[] args) {
 //        Faça um programa que preenche uma matriz 4 x 4 com o produto do valor da
 //        linha e da coluna de cada elemento. Em seguida, imprima na tela a matriz.
+        int[][] matriz = new int[4][4];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                matriz[i][j] = i * j;
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
